c31f7d56925310223f38aa2ddc5c89429287bdf3
Esquivel Jesus's (Yisus) <esquiveljesuss925@gmail.com>
2026-01-31 18:57:55 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve '..

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,16 +1,7 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-<<<<<<< HEAD
     '@tailwindcss/postcss': {},
   },
-}
-
-export default config
-=======
-    tailwindcss: {},
-  },
 };
-
 export default config;
->>>>>>> f2839bc (Initialized workspace with Firebase Studio)
